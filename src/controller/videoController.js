08c6c638ad5d3effb5fd54df4ae527a6f4bc73ca
bona <bona2324@gmail.com>
2022-05-09 19:18:18 +0900

    templates
    add & modify

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,6 +1,45 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send(`Watch Video #${req.params.id}`);
-export const edit = (req, res) => res.send("Edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 4,
+        createdAt: "10 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 4,
+        comments: 2,
+        createdAt: "7 minutes ago",
+        views: 50,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 1,
+        createdAt: "3 minutes ago",
+        views: 1,
+        id: 3,
+    },
+];
+export const trending = (req, res) => {
+    return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+    const { id } = req.params; //! (같은 코드) const id = req.params.id
+    const video = videos[id - 1]; //^ 리스트는 0부터 세지만 id는 1부터 시작하니까
+    res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    res.render("Edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { upload, see, edit, deleteVideo } from "../controller/videoController";
+import { upload, watch, getEdit, postEdit, deleteVideo } from "../controller/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see); //^ id자리에 숫자만 올 수 있도록!!
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch); //^ id자리에 숫자만 올 수 있도록!!
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
