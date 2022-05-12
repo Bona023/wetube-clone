f0501a371e6c96cdfa128f606d8e35bf4511573c
bona <bona2324@gmail.com>
2022-05-12 14:06:32 +0900

    modify
    임시 데이터 삭제, 필요 없는 코드 삭제, 함수 이름 변경

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,20 +1,17 @@
-export const trending = (req, res) => {
-    return res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+    return res.render("home", { pageTitle: "Home" });
 };
 export const watch = (req, res) => {
     const { id } = req.params; //! (같은 코드) const id = req.params.id
-    const video = videos[id - 1]; //^ 리스트는 0부터 세지만 id는 1부터 시작하니까
-    res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+    res.render("watch", { pageTitle: `Watching:` });
 };
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    res.render("Edit", { pageTitle: `Editing: ${video.title}`, video });
+    res.render("Edit", { pageTitle: `Editing:` });
 };
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -25,13 +22,5 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
     const { title } = req.body;
-    const newVideo = {
-        title,
-        rating: 0,
-        comments: 0,
-        createdAt: "just now",
-        id: videos.length + 1,
-    };
-    videos.push(newVideo);
     return res.redirect("/");
 };
