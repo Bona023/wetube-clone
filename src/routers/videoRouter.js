f3dc5e96104c4c4de6b129840c24287a21a3fe34
bona <bona2324@gmail.com>
2022-05-12 11:05:07 +0900

    connect DB

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { upload, watch, getEdit, postEdit, deleteVideo } from "../controller/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload } from "../controller/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch); //^ id자리에 숫자만 올 수 있도록!!
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
