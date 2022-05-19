d0a0c2c0d84d5932d63fbd049143109feb975785
bona <bona2324@gmail.com>
2022-05-19 17:49:39 +0900

    make : delete, search
    delete기능 만들기
    search page 만들기

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { watch, getEdit, postEdit, getUpload, postUpload } from "../controller/videoController";
+import { watch, getEdit, postEdit, deleteVideo, getUpload, postUpload } from "../controller/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch); //^ id자리에 숫자만 올 수 있도록!!
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
