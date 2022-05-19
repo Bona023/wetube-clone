7eff28ba336bf66376b82fdffe7dba887cb5ce39
bona <bona2324@gmail.com>
2022-05-19 12:10:33 +0900

    make : update, edit
    update, edit 만들기
    error 처리하기
    static function 만들기
    기타 여러가지 수정

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,17 +1,37 @@
-export const home = (req, res) => {
-    return res.render("home", { pageTitle: "Home" });
+import Video from "../models/video";
+
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const { id } = req.params; //! (같은 코드) const id = req.params.id
-    res.render("watch", { pageTitle: `Watching:` });
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not Found" });
+    }
+    return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    res.render("Edit", { pageTitle: `Editing:` });
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not Found" });
+    }
+    res.render("Edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id });
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not Found" });
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags),
+    });
     return res.redirect(`/videos/${id}`);
 };
 
@@ -20,7 +40,19 @@ export const getUpload = (req, res) => {
         pageTitle: "Upload Video",
     });
 };
-export const postUpload = (req, res) => {
-    const { title } = req.body;
-    return res.redirect("/");
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: Video.formatHashtags(hashtags),
+        });
+        return res.redirect("/");
+    } catch (error) {
+        return res.redirect("upload", {
+            pageTitle: "Upload Video",
+            errorMessage: error._message,
+        });
+    }
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { watch, getEdit, postEdit, getUpload, postUpload } from "../controller/v
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch); //^ id자리에 숫자만 올 수 있도록!!
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch); //^ id자리에 숫자만 올 수 있도록!!
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
