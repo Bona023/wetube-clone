6925a3795451aafcf14e585138ef6260d336082e
bona <bona2324@gmail.com>
2022-05-28 16:59:19 +0900

    login, log out
    login, log out 만들기
    github으로 로그인하기

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "cross-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -42,7 +43,7 @@ export const getLogin = (req, res) => {
 export const postLogin = async (req, res) => {
     const { username, password } = req.body;
     const pageTitle = "Login";
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username, socialOnly: false });
     if (!user) {
         return res.status(400).render("login", {
             pageTitle,
@@ -61,8 +62,88 @@ export const postLogin = async (req, res) => {
     return res.redirect("/");
 };
 
-export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
+export const startGithubLogin = (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        allow_signup: false,
+        scope: "read:user user:email", //^ 반드시 띄어쓰기로 구분할 것!!
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+};
 
-export const logout = (req, res) => res.send("Log out");
+export const finishGithubLogin = async (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code,
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await (
+        await fetch(finalUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+            },
+        })
+    ).json();
+    if ("access_token" in tokenRequest) {
+        const { access_token } = tokenRequest;
+        const apiUrl = "https://api.github.com";
+        const userData = await (
+            await fetch(`${apiUrl}/user`, {
+                headers: {
+                    Authorization: `token ${access_token}`,
+                },
+            })
+        ).json();
+        console.log(userData);
+        const emailData = await (
+            await fetch(`${apiUrl}/user/emails`, {
+                headers: {
+                    Authorization: `token ${access_token}`,
+                },
+            })
+        ).json();
+        const emailObj = emailData.find((email) => email.primary === true && email.verified === true);
+        if (!emailObj) {
+            //todo : set notification
+            return res.redirect("/login");
+        }
+        let user = await User.findOne({ email: emailObj.email });
+        //^ github으로 로그인한 사람이 기존에 가입된 사람인지 확인 (email로 가입했는지 github으로 가입했는지 구분 X)
+        if (!user) {
+            //^ 지금 github으로 로그인한 사람이 기존 가입자가 아니면 계정 생성
+            //^ 새로 생성한 계정 내용으로 user를 재정의 (그래서 let 사용)
+            user = await User.create({
+                avatarUrl: userData.avatar_url,
+                name: userData.name,
+                email: emailObj.email,
+                username: userData.login,
+                password: "",
+                socialOnly: true,
+                location: userData.location,
+            });
+        }
+        //^ 지금 github으로 로그인한 사람이 기존 가입자라서
+        //^ 아니면 새로 계정을 생성해서
+        //^ 로그인 시켜주고 로그인 상태로 홈으로 redirect
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
+    } else {
+        return res.redirect("/login");
+    }
+};
+
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
+
+export const edit = (req, res) => res.send("Edit User");
 export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controller/userController";
+import { edit, logout, startGithubLogin, finishGithubLogin, see } from "../controller/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
