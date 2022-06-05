0aba6a4e81ac94cf3dfbe69e4eb7cd975ff32796
bona <bona2324@gmail.com>
2022-06-05 13:48:48 +0900

    play & pause

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,23 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
+
+const handelMute = (e) => {};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handelMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
